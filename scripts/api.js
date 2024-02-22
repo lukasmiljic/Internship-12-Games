8ca7a957c9e7720af2409d1a61341d78619cbe67
lukasmiljic <smiljicluka13@gmail.com>
2024-02-22 21:57:27 +0100

    added code for task 1

--- scripts/api.js
+++ scripts/api.js
@@ -1,10 +1,11 @@
 const apiUrl = "https://api.rawg.io/api";
-const apiKey = "464bc085dbbf4f33bcb2ccb39d36a6ec";
+const apiKey = "064c6f5b9e0f4e6c961f35e4dd0a52ff";
 
 async function fetchData(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
+    console.log(data.results);
     return data.results;
   } catch (error) {
     console.error(error);
@@ -13,6 +14,6 @@ async function fetchData(url) {
 
 export async function fetchGamesOrderMetacritic() {
   return fetchData(
-    `${apiUrl}/games?=key${apiKey}&ordering=-metacritic&page_size=20`
+    `${apiUrl}/games?key=${apiKey}&ordering=-metacritic&page_size=20`
   );
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,39 @@
 import { fetchGamesOrderMetacritic } from "./api.js";
+fetchGamesOrderMetacritic();
+
+function generateContainer(games, title) {
+    const task = document.createElement("div");
+    task.classList.add("task");
+
+    const taskTitle = document.createElement("h2");
+    taskTitle.classList.add("task-title");
+    taskTitle.textContent = title;
+    task.appendChild(taskTitle);
+
+    const gamesContainer = document.createElement("div");
+    gamesContainer.classList.add("games-container");
+    task.appendChild(gamesContainer);
+
+    games.forEach((game) => {
+        let artwork = game.background_image;
+        if (artwork === null) {
+            artwork = "../assets/no_image.png";
+        }
+        const gameCard = document.createElement("div");
+        gameCard.classList.add("game-card");
+        gamesContainer.appendChild(gameCard);
+        gameCard.innerHTML = `
+      <img class="game-img" src="${artwork}" />
+        <div class="game-title">${game.name}</div>
+        <div class="game-release-date">${game.released}</div>
+        <div class="game-rating">${game.metacritic}/100</div>
+      `;
+    });
+    document.body.appendChild(task);
+}
+
+fetchGamesOrderMetacritic()
+    .then((games) =>
+        generateContainer(games, "Task 1. - Metacritic Top 20 Games")
+    )
+    .catch(console.error);
